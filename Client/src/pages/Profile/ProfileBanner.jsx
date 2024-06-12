394e3a3af14ebfa715cde0bb42309898693d4ae7
Tanner Davison <tanner.davison95@gmail.com>
2024-06-11 20:53:39 -0600

    final save for router cleanup

--- Client/src/pages/Profile/ProfileBanner.jsx
+++ Client/src/pages/Profile/ProfileBanner.jsx
@@ -54,11 +54,11 @@ const ProfileBanner = ({ userData }) => {
     setLoading(true);
     const formData = new FormData();
     formData.append("file", file);
-    // Add the file to the FormData
+
     try {
       const response = await fetch("/pixel-pushers/upload/ProfilePhoto", {
         method: "POST",
-        body: formData, // Send FormData instead of JSON
+        body: formData, 
         credentials: "include",
       });
       if (response.ok) {
